8de36e9937c50cc2b96a7c8cfe9769606e899fc1
John Mitchell <wsi_john@yahoo.com.ph>
2021-05-13 13:51:09 +0800

    router

--- server.go
+++ server.go
@@ -12,10 +12,10 @@ import (
 func handleRequests() {
     db.Init()
 
-    myRouter := mux.NewRouter().StrictSlash(true)
-    myRouter.HandleFunc("/", handler.ReturnHome)
-    myRouter.HandleFunc("/price", handler.ReturnAllPrice)
-    log.Fatal(http.ListenAndServe(":16000", myRouter))
+    router := mux.NewRouter().StrictSlash(true)
+    router.HandleFunc("/", handler.ReturnHome)
+    router.HandleFunc("/price", handler.ReturnAllPrice)
+    log.Fatal(http.ListenAndServe(":16000", router))
 }
 
 func main() {
